a4312f395f209fe2df3b8b40083d14217c3438a8
Максимилиан <maxsimilian560@gmail.com>
2026-05-29 23:27:12 +0300

    Fixed critical error

--- modules/include/Kernel/interPoint.h
+++ modules/include/Kernel/interPoint.h
@@ -1,16 +1,14 @@
 #pragma once
 
-//_UNICODE
 
 #ifndef RinegineLib
-int rg_main();
 #ifdef RG_UTF
 int wmain(int argc, wchar_t *argv[]) {
-  return Rinegine::Kernel::InterPoint(argc, argv, rg_main);
+  return Rinegine::Kernel::InterPoint(argc, argv, Rinegine::Kernel::rg_main);
 }
 #else
 int main(int argc, char *argv[]) {
-  return Rinegine::Kernel::InterPoint(argc, argv, rg_main);
+  return Rinegine::Kernel::InterPoint(argc, argv, Rinegine::Kernel::rg_main);
 }
 #endif
 #endif
